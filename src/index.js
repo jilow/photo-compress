1645c34aa7f8642d62ab22917990333cbb7cbc8b
Jiaan <jilow@users.noreply.github.com>
2022-06-19 18:08:12 +0000

    Add title, description and tag inputs

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const Image = require('./image');
 let watermark;
 
 const rel = (relativePath) => path.join(__dirname, relativePath)
-const throwOnFailed = (promise) => promise.then(err => { if (err) throw err })
+const throwOnFailed = (promise) => promise.then(err => { if (err) throw new Error(err) })
 
 const db = new JSONdb(rel('../processed/data.json'));
 
@@ -20,50 +20,57 @@ app.get('/', (req, res) => {
 });
 
 app.post('/upload', async (req, res) => {
-    // TODO: process the file upload
     if (!req.files) {
         return res.status(400).send('No files were uploaded.');
     }
 
+    const { title, description, tags } = req.body;
     const { data, name } = req.files.photo;
 
-    try {
+    const image = new Image(data)
+    const nameNoExt = name.split('.')[0];
 
-        const image = new Image(data)
-        const nameNoExt = name.split('.')[0];
+    const metadata = await image.metadata()
+    const tinyBuffer = await image.toJpeg(28, 50)
+    const smallBuffer = await image.toJpeg(450, 80)
+    const largeBuffer = await image.toJpeg(1200, 95, watermark, true)
 
-        const metadata = await image.metadata()
-        const tinyBuffer = await image.toJpeg(28, 50)
-        const smallBuffer = await image.toJpeg(450, 80)
-        const largeBuffer = await image.toJpeg(1200, 95, watermark, true)
+    const smallPath = `../processed/${nameNoExt}_450px.jpeg`;
+    const largePath = `../processed/${nameNoExt}_1200px.jpeg`;
 
-        const smallPath = `../processed/${nameNoExt}_450px.jpeg`;
-        const largePath = `../processed/${nameNoExt}_1200px.jpeg`;
+    await throwOnFailed(fs.writeFile(rel(smallPath), smallBuffer))
+    await throwOnFailed(fs.writeFile(rel(largePath), largeBuffer))
 
-        await throwOnFailed(fs.writeFile(rel(`../processed/${nameNoExt}_450px.jpeg`), smallBuffer))
-        await throwOnFailed(fs.writeFile(rel(`../processed/${nameNoExt}_1200px.jpeg`), largeBuffer))
+    const tinyBase64 = 'data:image/jpg;base64,' + tinyBuffer.toString('base64')
+    
+    const tagsArr = tags.replace(/\s/g, '').split(',')
 
-        const tinyBase64 = 'data:image/jpg;base64, ' + tinyBuffer.toString('base64')
+    const rows = db.get('data') || [];
+    console.log(rows)
+    rows.push({
+        id: rows.length + 1,
+        title: title || nameNoExt,
+        filename: name,
+        description,
+        tags: tagsArr,
+        base64: tinyBase64,
+        small: smallPath,
+        large: largePath,
+        metadata,
+    });
+    db.set('data', rows);
 
-        const rows = db.get('data') || [];
-        rows.push({
-            id: rows.length + 1,
-            base64: tinyBase64,
-            small: smallPath,
-            large: largePath,
-            metadata,
-        });
-        db.set('data', rows);
-        db.sync();
+    const tagRows = new Set(db.get('tags') || []);
+    tagsArr.forEach(tag => tagRows.add(tag));
+    db.set('tags', Array.from(tagRows))
+    
+    db.sync();
 
-        res.send({
-            status: 'success',
-            message: `Processed image ${nameNoExt}`,
-            data: metadata
-        });
-    } catch (err) {
-        res.status(500).send(err);
-    }
+    res.send({
+        status: 'success',
+        message: `Processed image ${nameNoExt}`,
+        data: metadata
+    });
 
 });
 
